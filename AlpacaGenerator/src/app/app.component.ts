b4be18d29ea5c30725c4bea9f67ac8c11e45b15b
Juan Pablo Gaviria Salazar <jgavirias13@gmail.com>
2023-05-06 18:11:35 -0500

    Adding Random functionality

--- AlpacaGenerator/src/app/app.component.ts
+++ AlpacaGenerator/src/app/app.component.ts
@@ -62,4 +62,60 @@ export class AppComponent {
   onElementSelected(element: ElementOption) {
     this.selectedOptions.set(this.currentHeader, element);
   }
+
+  generateRandomAlpaca() {
+    const accessoriesId = this.getRandomInRange(0, 4);
+    const backgroundId = this.getRandomInRange(1, 18);
+    const earsId = this.getRandomInRange(1, 3);
+    const eyesId = this.getRandomInRange(1, 6);
+    const hairId = this.getRandomInRange(1, 7);
+    const legId = this.getRandomInRange(1, 6);
+    const mouthId = this.getRandomInRange(1, 5);
+    const neckId = this.getRandomInRange(1, 4);
+
+    if (accessoriesId !== 0) {
+      this.selectedOptions.set(
+        Headers.ACCESORIES,
+        alpacaOptions
+          .get(Headers.ACCESORIES)!
+          .find((e) => e.id === accessoriesId)!
+      );
+    } else {
+      this.selectedOptions.delete(Headers.ACCESORIES);
+    }
+    this.selectedOptions.set(
+      Headers.BACKGROUND,
+      alpacaOptions.get(Headers.BACKGROUND)!.find((e) => e.id === backgroundId)!
+    );
+    this.selectedOptions.set(
+      Headers.EARS,
+      alpacaOptions.get(Headers.EARS)!.find((e) => e.id === earsId)!
+    );
+    this.selectedOptions.set(
+      Headers.EYES,
+      alpacaOptions.get(Headers.EYES)!.find((e) => e.id === eyesId)!
+    );
+    this.selectedOptions.set(
+      Headers.HAIR,
+      alpacaOptions.get(Headers.HAIR)!.find((e) => e.id === hairId)!
+    );
+    this.selectedOptions.set(
+      Headers.LEG,
+      alpacaOptions.get(Headers.LEG)!.find((e) => e.id === legId)!
+    );
+    this.selectedOptions.set(
+      Headers.MOUTH,
+      alpacaOptions.get(Headers.MOUTH)!.find((e) => e.id === mouthId)!
+    );
+    this.selectedOptions.set(
+      Headers.NECK,
+      alpacaOptions.get(Headers.NECK)!.find((e) => e.id === neckId)!
+    );
+  }
+
+  getRandomInRange(min: number, max: number): number {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
 }
--- AlpacaGenerator/src/app/app.module.ts
+++ AlpacaGenerator/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgIconsModule } from '@ng-icons/core';
+import { matShuffleOutline } from '@ng-icons/material-icons/outline'
+import { matDownloadOutline } from '@ng-icons/material-icons/outline'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SelectorComponent } from './selector/selector/selector.component';
@@ -16,7 +18,8 @@ import { VisualAlpacaComponent } from './visual-alpaca/visual-alpaca.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgIconsModule.withIcons({ matShuffleOutline, matDownloadOutline })
   ],
   providers: [],
   bootstrap: [AppComponent]
