bf2e04359b5db8b0a08a8dd9fc549769cd91b185
Juan Pablo Gaviria Salazar <jgavirias13@gmail.com>
2023-05-06 16:52:20 -0500

    First MVP - The changing elements is working

--- AlpacaGenerator/src/app/visual-alpaca/visual-alpaca.component.ts
+++ AlpacaGenerator/src/app/visual-alpaca/visual-alpaca.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ElementOption } from '../model/ElementOption';
+import { Headers } from '../model/Headers';
 
 @Component({
   selector: 'app-visual-alpaca',
@@ -6,5 +8,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./visual-alpaca.component.scss']
 })
 export class VisualAlpacaComponent {
-
+  @Input() selectedOptions!: Map<Headers, ElementOption>;
+  headers = Headers;
 }
